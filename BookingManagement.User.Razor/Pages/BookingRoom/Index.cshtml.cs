685b3f25a26b389912ba04bdcd04b3b52acea39e
lap03 <tinle7347@gmail.com>
2025-04-03 23:15:23 +0700

    fix bookingRoom index

--- BookingManagement.User.Razor/Pages/BookingRoom/Index.cshtml.cs
+++ BookingManagement.User.Razor/Pages/BookingRoom/Index.cshtml.cs
@@ -9,6 +9,7 @@ using BookingManagement.Repositories.Data;
 using BookingManagement.Repositories.Models;
 using Microsoft.AspNetCore.Authorization;
 using BookingManagement.Services.Interfaces;
+using System.Security.Claims;
 
 namespace BookingManagement.User.Razor.Pages.BookingRoom
 {
@@ -16,23 +17,29 @@ namespace BookingManagement.User.Razor.Pages.BookingRoom
     public class IndexModel : PageModel
     {
         private readonly IRoomService _roomService;
+        private readonly IBookingService _bookingService;
 
-        private readonly BookingManagement.Repositories.Data.FptuRoomBookingContext _context;
-
-        public IndexModel(BookingManagement.Repositories.Data.FptuRoomBookingContext context, IRoomService roomService)
+        public IndexModel(IBookingService bookingService, IRoomService roomService)
         {
-            _context = context;
+            _bookingService = bookingService;
             _roomService = roomService;
         }
 
         public IList<Booking> Booking { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            Booking = await _context.Bookings
-                .Include(b => b.Room)
-                .Include(b => b.TimeSlot)
-                .Include(b => b.User).ToListAsync();
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!string.IsNullOrEmpty(userIdClaim) && int.TryParse(userIdClaim, out int loggedInUserId))
+            {
+                Booking = (IList<Booking>)await _bookingService.GetBookingsByUserIdAsync(loggedInUserId);
+            }
+            else
+            {
+                TempData["message"] = "User chưa loggin!";
+                return RedirectToPage("/Login/Index");
+            }
+            return Page();    
         }
 
         public string GetStatusText(int status)
